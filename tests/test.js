ce290a2281d9e0c3d1c3fe7c247c9083fa3d019a
Daniel Sapkota <daniel@waterbot.xyz>
2022-08-20 20:30:30 -0700

    gitignore config

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,4 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-
-const WETH_ADDY = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 let weth_contract = new ethers.Contract(WETH_ADDY,erc20ABI);
