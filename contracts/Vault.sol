1f6626f83538df4c82613ff64a0a0062f717362b
Daniel Sapkota <daniel@waterbot.xyz>
2022-08-20 20:27:26 -0700

    Creating config file and defining the flow'

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -1,22 +1,29 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Vault is ERC1155{
+contract Vault is ERC1155, ReentrancyGuard{
     string public VAULT_NAME;
     address public WETH;
     bool public external_lp_enabled;
     address public VAULT_MANAGER;
+    address[] public collections;
+    uint256[] public ltvs;
+    uint256 initialAPR;
 
     modifier onlyVaultManager {
         require (msg.sender == VAULT_MANAGER);
         _;
     }
 
-    constructor(string memory _VAULT_NAME, address _WETH, address _VAULT_MANAGER, bool _external_lp_enabled) ERC1155("https://example.com"){
+    constructor(string memory _VAULT_NAME, address _WETH, address _VAULT_MANAGER, address[] memory _collections, uint256[] memory _ltvs, uint256 _initialAPR, bool _external_lp_enabled) ERC1155("https://example.com"){
         VAULT_NAME = _VAULT_NAME;
         WETH = _WETH;
         VAULT_MANAGER = _VAULT_MANAGER;
         external_lp_enabled = _external_lp_enabled;
+        collections = _collections;
+        ltvs = _ltvs;
+        initialAPR = _initialAPR;
     }
 
 
--- contracts/VaultManager.sol
+++ contracts/VaultManager.sol
@@ -12,13 +12,13 @@ contract VaultManager{
         WETH = _WETH;
     }
 
-    function createVault( string calldata _VAULT_NAME, bool _external_lp_enabled, uint256 liquidityAdded) public returns (address) {
+    function createVault( string calldata _VAULT_NAME, address[] memory _collections, uint256[] memory _ltvs, uint256 _initialAPR, bool _external_lp_enabled, uint256 liquidityAdded) public returns (address) {
         if (liquidityAdded > 0){
             (bool success, bytes memory data) = WETH.call(abi.encodeWithSelector(0x23b872dd, msg.sender, address(this), liquidityAdded));
             require(success, "Cannot transfer DAI");
         }
         
-        Vault vault = new Vault(_VAULT_NAME, WETH, address(this), _external_lp_enabled);
+        Vault vault = new Vault(_VAULT_NAME, WETH, address(this), _collections, _ltvs, _initialAPR, _external_lp_enabled);
         vaults.push(address(vault));
 
         return address(vault);
