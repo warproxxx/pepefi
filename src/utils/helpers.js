23cabd167464a66aa279ffeab8902983f6000218
June Liang <liangj10@unlv.nevada.edu>
2022-08-25 20:28:25 -0700

    addVault done

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -11,3 +11,9 @@ export const toHex = (num) => {
     const val = Number(num);
     return "0x" + val.toString(16);
 };
+
+export const camelCaseToSpace = (text) =>{
+  const result = text.replace(/([A-Z])/g, " $1");
+  const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+  return finalResult;
+}
