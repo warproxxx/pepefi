04dff82f63160d4a362cce8c88fb7d8d4262e8e9
June Liang <liangj10@unlv.nevada.edu>
2022-08-26 20:38:30 -0700

    changing my loans to a table for now

--- src/components/MyLoans/MyLoansPopUp.tsx
+++ src/components/MyLoans/MyLoansPopUp.tsx
@@ -16,7 +16,9 @@ import {
   Checkbox,
   Tooltip,
   Divider,
-  Grid
+  Grid,
+  ThemeProvider,
+  createTheme,
 } from "@mui/material";
 
 import { styled, experimental_sx as sx } from '@mui/system';
@@ -31,6 +33,7 @@ import Image from "next/image";
 
 import { myLoans } from "src/data/myLoans";
 
+import MUIDataTable from "mui-datatables";
 
 
 
@@ -92,56 +95,253 @@ const Transition = forwardRef(function Transition(props, ref) {
 
 const dataRows = [
   {
-    name: 'NFT Name',
+    name: 'name',
     dataName: 'name',
     unit: '',
+    label: 'NFT Name',
+    options: {
+      filter: true,
+      sort: true,
+    }
   },
   {
     name: 'collection',
     dataName: 'collection',
     unit: '',
+    label: 'collection',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Lended Vault',
+    name: 'lendedVault',
     dataName: 'lendedVault',
     unit: '',
+    label: 'Lended Vault',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
+
   {
     name: 'APR',
     dataName: 'APR',
     unit: '%',
+    label: 'APR',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Loan Amount',
+    name: 'loanAmount',
     dataName: 'loanAmount',
     unit: 'WETH',
+    label: 'Loan Amount',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Loan Date',
+    name: 'loanDate',
     dataName: 'loanDate',
     unit: '',
+    label: 'Loan Date',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Remaining Days',
+    name: 'remainingDays',
     dataName: 'remainingDays',
     unit: 'days',
+    label: 'Remaining Days',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Repayment Amount',
+    name: 'repaymentAmount',
     dataName: 'repaymentAmount',
     unit: 'WETH',
+    label: 'Repayment Amount',
+    options: {
+      filter: true,
+      sort: true,
+     }
   },
   {
-    name: 'Action',
+    name: 'action',
     dataName: '',
     unit: '',
+    label: 'Action',
+    options: {
+      filter: false,
+      sort: false,
+     }
   }
 ]
 
+const dataRows2 = [
+  {
+    name: 'nftPicture',
+    dataName: 'nftPicture',
+    unit: '',
+    label: 'NFT Picture',
+    options: {
+      filter: false,
+      sort: false,
+    }
+  },
+  {
+    name: 'name',
+    dataName: 'name',
+    unit: '',
+    label: 'NFT Name',
+    options: {
+      filter: true,
+      sort: true,
+    }
+  },
+  {
+    name: 'collection',
+    dataName: 'collection',
+    unit: '',
+    label: 'collection',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'lendedVault',
+    dataName: 'lendedVault',
+    unit: '',
+    label: 'Lended Vault',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+
+  {
+    name: 'APR',
+    dataName: 'APR',
+    unit: '%',
+    label: 'APR',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'loanAmount',
+    dataName: 'loanAmount',
+    unit: 'WETH',
+    label: 'Loan Amount',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'loanDate',
+    dataName: 'loanDate',
+    unit: '',
+    label: 'Loan Date',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'remainingDays',
+    dataName: 'remainingDays',
+    unit: 'days',
+    label: 'Remaining Days',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'repaymentAmount',
+    dataName: 'repaymentAmount',
+    unit: 'WETH',
+    label: 'Repayment Amount',
+    options: {
+      filter: true,
+      sort: true,
+     }
+  },
+  {
+    name: 'action',
+    dataName: '',
+    unit: '',
+    label: 'Action',
+    options: {
+      filter: false,
+      sort: false,
+     }
+  }
+]
+
+const options = {
+  selectableRowsHideCheckboxes:true,
+  filterType: 'checkbox',
+  pagination: false,
+  selectableRows: 'none',
+  viewColumns: false,
+}
+
+const getMuiTheme = () => createTheme({
+  components: {
+      MuiPaper:{
+        styleOverrides:{
+          root: { backgroundColor:'transparent',color:'white !important',fontFamily:'DM Mono !important',
+          "boxShadow":"0px 2px 5px 1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 20%), 0px 1px 5px 0px rgb(0 0 0 / 20%)",
+          padding:'20px'
+          }
+        }
+      },
+      MuiTableRow:{
+        styleOverrides:{
+          root: { backgroundColor:'transparent',color:'white' }
+        }
+      },
+      MuiTableCell:{
+        styleOverrides:{
+          root: { backgroundColor:'transparent !important',color:'white !important',borderBottom:'1px solid rgb(255 255 255 / 40%);'},
+          head: {
+            '& div[class*="sort"]':{
+              color:'white !important',
+            },
+            '& svg':{
+              color:'white !important',
+            }
+          },
+          body:{
+            fontFamily:'inherit !important'
+          }
+        },
+      },
+      MuiButtonBase:{
+        styleOverrides:{
+          root: { backgroundColor:'transparent !important',color:'white !important' }
+        }
+      },
+      
+  },
+
+})
 
 export const MyLoansPopUp = (props:any) => {
     const [activeStep, setActiveStep] = useState(0);
-
     return (
         <Dialog
         open={props.open}
@@ -150,7 +350,7 @@ export const MyLoansPopUp = (props:any) => {
         onClose={props.handleClose}
         PaperProps={{
           sx:{
-            minWidth:'1200px',
+            minWidth:'1400px',
             background: "#18181833",
             boxShadow: "3px 11px 3px rgba(0, 0, 0, 0.25)",
             backdropFilter:"blur(30px)",
@@ -163,7 +363,7 @@ export const MyLoansPopUp = (props:any) => {
         >
         <DialogTitle sx={{color:'white',alignSelf: 'flex-start'}} variant="h2">{"My Loans"}</DialogTitle>
         <DialogContent sx={{width:'100%',padding:'20px'}}>
-        {
+        {/* {
           myLoans.map((loan,index)=>{
             return(
               <Box 
@@ -182,7 +382,7 @@ export const MyLoansPopUp = (props:any) => {
                         <Grid item xl={4} lg={4} md={4} sm={6} xs={12} sx={{margin:'0px',padding:'0px !important'}} key={index}>
                         <Box sx={{width:'100%',height:'100%',display:'flex',justifyContent:'flex-start',alignItems:'center'}}>
                           {
-                            row.name == "Action" ?
+                            row.label == "Action" ?
                               <Button variant="contained" sx={{
                                 width:'50%',
                                 fontSize:'20px',
@@ -195,15 +395,13 @@ export const MyLoansPopUp = (props:any) => {
                             :
                             <Box>
                               <MyLoansPopUpLabel2Typography>
-                                {row.name}
+                                {row.label}
                               </MyLoansPopUpLabel2Typography>
                               <MyLoansPopUpData2Typography>
                                 {`${loan[row.dataName]} ${row.unit}`}
                               </MyLoansPopUpData2Typography>                  
                             </Box>
                           }
-  
-  
                         </Box>
                       </Grid>
                       )
@@ -216,6 +414,14 @@ export const MyLoansPopUp = (props:any) => {
             )
           })
         }
+        <Box sx={{my:'40px'}}/> */}
+          <ThemeProvider theme={getMuiTheme()}>
+            <MUIDataTable
+              data={myLoans}
+              columns={dataRows2}
+              options={options}
+            />
+          </ThemeProvider>
 
         </DialogContent>
         </Dialog>
