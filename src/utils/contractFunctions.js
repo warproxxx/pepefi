b1b3e07e43ba99847456c7fc8d388af26bd3ba5f
June Liang <liangj10@unlv.nevada.edu>
2022-08-27 17:08:48 -0700

    fix bug

--- src/utils/contractFunctions.js
+++ src/utils/contractFunctions.js
@@ -2,9 +2,6 @@ import { store } from "../app/store";
 import { ethers } from "ethers";
 import {PEPEAUCTION_ABI, PEPEFIORACLE_ABI, VAULT_ABI, VAULTMANAGER_ABI, VAULTUTILS_ABI, ORACLE_CONTRACT, VAULT_MANAGER, WETH, ERC20_ABI} from "../config"
 
-let wallets = store.getState().wallets;
-let signer = store.getState().wallets.signer;
-let provider = signer.provider;
 export const showWallets = () => {
     console.log(wallets);
 }
@@ -14,6 +11,7 @@ export const contractStuff = () => {
 }
 
 async function approve_and_spend(addy, abi, signer){
+    let provider = signer.provider;
     let contract = new ethers.Contract( addy, abi, signer )
     let x = await contract.allowance(provider.address, nftfi)
 
@@ -23,7 +21,10 @@ async function approve_and_spend(addy, abi, signer){
 }
 
 export const addVault = async (details) => {
-    let vm = new ethers.Contract( VAULT_MANAGER , VAULTMANAGER_ABI , signer )
+    let wallets = store.getState().wallets;
+    let signer = wallets.library.getSigner()
+    let provider = signer.provider;
+    let vm = new ethers.Contract( VAULT_MANAGER , VAULTMANAGER_ABI , signer)
 
     if (details.initialVaultDeposit > 0){
         await approve_and_spend(WETH, ERC20_ABI, signer)
